bd97fe5aa1053e1143ac105a959ea86a4b9245ea
DaEnoch <ei.yanezs@uea.edu.ec>
2026-02-01 03:10:59 -0500

    Semana 8
    Actividades Semana 8

---
+++ Semana 8/NavegadorPila/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Navegador
+{
+    private Stack<string> historial = new Stack<string>();
+
+    public void VisitarPagina(string pagina)
+    {
+        historial.Push(pagina);
+        Console.WriteLine("Página actual: " + pagina);
+    }
+
+    public void Retroceder()
+    {
+        if (historial.Count > 1)
+        {
+            historial.Pop();
+            Console.WriteLine("Retrocediendo...");
+            Console.WriteLine("Página actual: " + historial.Peek());
+        }
+        else
+        {
+            Console.WriteLine("No hay páginas anteriores.");
+        }
+    }
+
+    public void MostrarHistorial()
+    {
+        Console.WriteLine("Historial de navegación:");
+        foreach (var pagina in historial)
+        {
+            Console.WriteLine(pagina);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Navegador nav = new Navegador();
+
+        nav.VisitarPagina("Inicio");
+        nav.VisitarPagina("Noticias");
+        nav.VisitarPagina("Contacto");
+
+        nav.Retroceder();
+        nav.MostrarHistorial();
+    }
+}
